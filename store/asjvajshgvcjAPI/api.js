9e6a9fc906085a69f8120f3d7ada9724d2177bfc
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 17:05:19 +0000

    Update store.

--- store/asjvajshgvcjAPI/api.js
+++ store/asjvajshgvcjAPI/api.js
@@ -1,76 +1,80 @@
-import axios from "axios";
+import axios from "axios"
 const asjvajshgvcjAPI = axios.create({
   baseURL: "https://asjvajshgvcj-6.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return asjvajshgvcjAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return asjvajshgvcjAPI.post(`/api/v1/login/`, payload);
+  return asjvajshgvcjAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return asjvajshgvcjAPI.post(`/api/v1/signup/`, payload);
+  return asjvajshgvcjAPI.post(`/api/v1/signup/`, payload)
 }
-
+function modules_files_uploads_list(payload) {
+  return asjvajshgvcjAPI.get(`/modules/files/uploads/`)
+}
+function modules_files_uploads_create(payload) {
+  return asjvajshgvcjAPI.post(`/modules/files/uploads/`, payload)
+}
+function modules_files_uploads_retrieve(payload) {
+  return asjvajshgvcjAPI.get(`/modules/files/uploads/${payload.id}/`)
+}
+function modules_files_uploads_update(payload) {
+  return asjvajshgvcjAPI.put(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_partial_update(payload) {
+  return asjvajshgvcjAPI.patch(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_destroy(payload) {
+  return asjvajshgvcjAPI.delete(`/modules/files/uploads/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/login/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return asjvajshgvcjAPI.get(`/rest-auth/logout/`);
+  return asjvajshgvcjAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/logout/`);
+  return asjvajshgvcjAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/password/change/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/password/reset/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/registration/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return asjvajshgvcjAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return asjvajshgvcjAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return asjvajshgvcjAPI.get(`/rest-auth/user/`);
+  return asjvajshgvcjAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return asjvajshgvcjAPI.put(`/rest-auth/user/`, payload);
+  return asjvajshgvcjAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return asjvajshgvcjAPI.patch(`/rest-auth/user/`, payload);
+  return asjvajshgvcjAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_files_uploads_list,
+  modules_files_uploads_create,
+  modules_files_uploads_retrieve,
+  modules_files_uploads_update,
+  modules_files_uploads_partial_update,
+  modules_files_uploads_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -82,4 +86,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
